8cd28bfe10590507893993f1330e1d491b4bdc6a
Jim Hughes <jhughes@ccri.com>
2021-03-10 13:57:18 -0500

    Adding more comments.
    Signed-off-by: Jim Hughes <jhughes@ccri.com>

--- src/test/scala/com/github/jnh5y/VarArgsTest.scala
+++ src/test/scala/com/github/jnh5y/VarArgsTest.scala
@@ -10,18 +10,23 @@ class VarArgsTest extends Specification {
 
   "VarArgs" should {
     "work with objects" in {
-      val objects = Array[AnyRef]("foo", new Integer(1))
+      val objects: Array[AnyRef] = Array[AnyRef]("foo", new Integer(1))
 
+      // This represents GT up to 24.0.
       // Prints:
       // Called arrayFunction with 2 inputs.
       //  Object 0:foo
       //  Object 1:1
       MyJavaClass.arrayFunction(objects)
 
+      /*  New GeoTools code in 25-RC / 25.0 */
       // Prints:
       // Called varargsFunction with 1 inputs.
       //  Object 0:[Ljava.lang.Object;@6b12d449
       MyJavaClass.varargsFunction(objects)
+
+      // The right way to call from Scala is
+      MyJavaClass.varargsFunction(objects: _*)
 
       ok
     }
@@ -36,9 +41,9 @@ class VarArgsTest extends Specification {
 
       MyJavaClass.arrayFunctionInteger(integers)
 
-      // Note that
+      // Note that the next line will not compile:
       // MyJavaClass.varargsFunctionInteger(integers)
-      // will not compile!
+
       // This means that the change from Integer[] -> Integer... will surface as a compiler error.
       MyJavaClass.varargsFunctionInteger(integers: _*)
 
